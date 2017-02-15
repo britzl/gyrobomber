4a4d8f5b8fad23e841e129ca93b8517907d4d2c3
Björn Ritzl <bjorn.ritzl@king.com>
2017-02-15 07:25:15 +0100

    Cleanup

--- gyro/src/gyro_ios.mm
+++ gyro/src/gyro_ios.mm
@@ -16,37 +16,6 @@ struct Gyro {
   GyroListener listener;
 } g_Gyro;
 
-static void ObjCToLua(lua_State*L, id obj)
-{
-    if ([obj isKindOfClass:[NSString class]]) {
-        const char* str = [((NSString*) obj) UTF8String];
-        lua_pushstring(L, str);
-    } else if ([obj isKindOfClass:[NSNumber class]]) {
-        lua_pushnumber(L, [((NSNumber*) obj) doubleValue]);
-    } else if ([obj isKindOfClass:[NSNull class]]) {
-        lua_pushnil(L);
-    } else if ([obj isKindOfClass:[NSDictionary class]]) {
-        NSDictionary* dict = (NSDictionary*) obj;
-        lua_createtable(L, 0, [dict count]);
-        for (NSString* key in dict) {
-            lua_pushstring(L, [key UTF8String]);
-            id value = [dict objectForKey:key];
-            ObjCToLua(L, (NSDictionary*) value);
-            lua_rawset(L, -3);
-        }
-    } else if ([obj isKindOfClass:[NSArray class]]) {
-        NSArray* a = (NSArray*) obj;
-        lua_createtable(L, [a count], 0);
-        for (int i = 0; i < [a count]; ++i) {
-            ObjCToLua(L, [a objectAtIndex: i]);
-            lua_rawseti(L, -2, i+1);
-        }
-    } else {
-        dmLogWarning("Unsupported payload value '%s' (%s)", [[obj description] UTF8String], [[[obj class] description] UTF8String]);
-        lua_pushnil(L);
-    }
-}
-
 int Gyro_PlatformStart(lua_State* L) {
     Gyro* gyro = &g_Gyro;
     
@@ -68,7 +37,6 @@ int Gyro_PlatformStart(lua_State* L) {
     gyro->motionManager = [[CMMotionManager alloc] init];
     gyro->motionManager.gyroUpdateInterval = 1.0/60.0;
     
-    //[gyro->motionManager startDeviceMotionUpdatesToQueue: [NSOperationQueue currentQueue]
     [gyro->motionManager startDeviceMotionUpdatesUsingReferenceFrame: CMAttitudeReferenceFrameXArbitraryCorrectedZVertical
                                                 toQueue: [NSOperationQueue currentQueue]
                                             withHandler: ^(CMDeviceMotion *data, NSError *error)
@@ -95,54 +63,13 @@ int Gyro_PlatformStart(lua_State* L) {
                                                 Vectormath::Aos::Quat rot(quat.x, quat.y, quat.z, quat.w);
                                                 dmScript::PushQuat(L, rot);
                                                 
-                                                CMRotationMatrix m4 = attitude.rotationMatrix;
-                                                Vectormath::Aos::Matrix4 matrix4;
-                                                matrix4.setElem(1, 1, m4.m11);
-                                                matrix4.setElem(1, 2, m4.m12);
-                                                matrix4.setElem(1, 3, m4.m13);
-                                                matrix4.setElem(2, 1, m4.m21);
-                                                matrix4.setElem(2, 2, m4.m22);
-                                                matrix4.setElem(2, 3, m4.m23);
-                                                matrix4.setElem(3, 1, m4.m31);
-                                                matrix4.setElem(3, 2, m4.m32);
-                                                matrix4.setElem(3, 3, m4.m33);
-                                                dmScript::PushMatrix4(L, matrix4);
-                                                
-                                                int ret = lua_pcall(L, 6, LUA_MULTRET, 0);
+                                                int ret = lua_pcall(L, 5, LUA_MULTRET, 0);
                                                 if (ret != 0) {
                                                     dmLogError("Error running gyro callback: %s", lua_tostring(L, -1));
                                                     lua_pop(L, 1);
                                                 }
                                                 assert(top == lua_gettop(L));
                                             }];
-    // [gyro->motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue]
-    //                            withHandler: ^(CMGyroData *gyroData, NSError *error)
-    //                                       {
-    //                                         CMRotationRate rotate = gyroData.rotationRate;
-    //                                         NSLog(@"rotation rate = [%f, %f, %f]", rotate.x, rotate.y, rotate.z);
-    //                                         
-    //                                         lua_State* L = gyro->listener.m_L;
-    //                                         int top = lua_gettop(L);
-    // 
-    //                                         lua_rawgeti(L, LUA_REGISTRYINDEX, gyro->listener.m_Callback);
-    // 
-    //                                         // Setup self
-    //                                         lua_rawgeti(L, LUA_REGISTRYINDEX, gyro->listener.m_Self);
-    //                                         lua_pushvalue(L, -1);
-    //                                         dmScript::SetInstance(L);
-    //                                         
-    //                                         lua_pushnumber(L, rotate.x);
-    //                                         lua_pushnumber(L, rotate.y);
-    //                                         lua_pushnumber(L, rotate.z);
-    // 
-    //                                         int ret = lua_pcall(L, 3, LUA_MULTRET, 0);
-    //                                         if (ret != 0) {
-    //                                             dmLogError("Error running gyro callback: %s", lua_tostring(L, -1));
-    //                                             lua_pop(L, 1);
-    //                                         }
-    //                                         assert(top == lua_gettop(L));
-    //                                         
-    //                                       }];
     dmLogInfo("GYRO HAS STARTED");
     return 1;
 }
